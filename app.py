e961d7751759e4002b3b15f31e5205a0c6b00e3c
runy1111 <serezha.koreshkov@gmail.com>
2025-08-15 05:02:05 +0300

    ed_bot_v2

--- app.py
+++ app.py
@@ -1,40 +1,27 @@
 import asyncio
+import os
 import datetime
-import time
-import tokens
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
-from dota_wr import get_info
 
+from dotenv import find_dotenv, load_dotenv
+load_dotenv(find_dotenv())
 
-bot = Bot(token=tokens.bot_token)
+from handlers.user_private import user_private_router
+
+ALLOWED_UPDATES = ["message", "edited_message"]
+
+bot = Bot(token=os.getenv('BOT_TOKEN'))
 dp = Dispatcher()
 
-
-@dp.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer('Привет, Питер')
-
-
-@dp.message()
-async def echo(message: types.Message, bot: Bot):
-    # await bot.send_message(message.from_user.ip, 'QWE')
-    if message.text[:3] == '/wr':
-        try:
-            await message.answer(get_info(message.text[3:]))
-        except AttributeError:
-            await message.answer('try one more time')
-    else:
-        # await bot.send_message(message.from_user.id, 'Ответ')
-        await message.answer(message.text)
-        # await message.reply(message.text)
+dp.include_router(user_private_router)
 
 
 async def main():
     print(datetime.datetime.now(), "Bot started!")
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 
 asyncio.run(main())
---
+++ handlers/user_private.py
@@ -0,0 +1,42 @@
+from aiogram import Router, types
+import aiohttp
+from aiogram.filters import CommandStart, Command
+from dota_wr import get_info
+import time
+from dotenv import find_dotenv, load_dotenv
+load_dotenv(find_dotenv())
+user_private_router = Router()
+
+
+@user_private_router.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer('Привет, Питер')
+
+
+@user_private_router.message(Command("menu"))
+async def menu_cmd(message: types.Message):
+    await message.answer("Меню:")
+
+
+@user_private_router.message(Command("wr"))
+async def check_wr(message: types.Message):
+    await message.reply("Напишите id игрока")
+
+    @user_private_router.message()
+    async def winrate(message: types.Message):
+        try:
+            await message.answer(get_info(message.text))
+        except AttributeError:
+            time.sleep(3)
+            try:
+                await message.answer(get_info(message.text))
+            except AttributeError:
+                time.sleep(3)
+                try:
+                    await message.answer(get_info(message.text))
+                except AttributeError:
+                    time.sleep(3)
+                    try:
+                        await message.answer(get_info(message.text))
+                    except AttributeError:
+                        await message.answer('try one more time')
